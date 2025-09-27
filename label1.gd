5a421a107ecc850d17b0883d0c52905a46705f1a
thecooldoggo <thecooldoggobusiness@gmail.com>
2025-09-27 15:02:18 -0400

    flower

--- label1.gd
+++ label1.gd
@@ -1,6 +1,6 @@
 extends Label
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	text = "Welcome to the slums..."
+	text = "You find yourself in a lifeless worlds"
 	show()
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	hide()
