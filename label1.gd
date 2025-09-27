557485119e8005f46d95e562822b6ded8a94ddd5
thecooldoggo <thecooldoggobusiness@gmail.com>
2025-09-27 14:57:00 -0400

    top level

--- label1.gd
+++ label1.gd
@@ -4,3 +4,12 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	show()
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	hide()
+
+
+func _on_area_2_popup_body_entered(body: Node2D) -> void:
+	text = "Concrete Jungle"
+	show()
+
+
+func _on_area_2_popup_body_exited(body: Node2D) -> void:
+	hide()
