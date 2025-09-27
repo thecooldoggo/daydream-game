4a321dba8309a91912f3f5d7ff8f7d548fdfe943
thecooldoggo <thecooldoggobusiness@gmail.com>
2025-09-27 13:22:15 -0400

    animations + more

--- player.gd
+++ player.gd
@@ -1,33 +1,39 @@
 extends CharacterBody2D
 
-const SPEED = 500.0
+const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 var IS_MOVING = false
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		IS_MOVING = true
-		animated_sprite.stop()
-	# Handle jump.
+		animated_sprite.play("JumpAni")
+
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		IS_MOVING = true
-		animated_sprite.stop()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# this is for movement and animations 
 	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-		IS_MOVING = true
-		animated_sprite.stop()
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	if is_on_floor():
+		if direction:
+			velocity.x = direction * SPEED
+			animated_sprite.play("walkAnimation")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			animated_sprite.play("idleAnimation")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		IS_MOVING = false
-		animated_sprite.play("idleAnimation")
+		velocity.x = direction * SPEED
+		animated_sprite.play("JumpAni")
 		
 	move_and_slide()
